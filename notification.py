350bad0e1d97b35da3ba2e5ddcd44bbd46df8006
spendyala <pvvssubrahmanyam@gmail.com>
2017-03-27 14:56:36 +0000

    One in night and one in the morning

--- notification.py
+++ notification.py
@@ -13,13 +13,20 @@ def send_notification(pattern, del_flag=False):
     redis_con = redis.Redis()
     get_keys = redis_con.keys(pattern)
     if del_flag:
-        redis_con.del(get_keys)
+        redis_con.delete(get_keys)
         return
     for each_time_key in get_keys:
         get_users_list = redis_con.hgetall(each_time_key)
         for each_user, each_data in get_users_list.items():
-            date = each_time_key[:10].decode('utf-8')
-            notification_sent_flag = redis_con.hget(date, each_user)
+            import pdb; pdb.set_trace()
+            if '1900-' not in each_time_key.decode('utf-8'):
+                date = each_time_key[:10].decode('utf-8')
+                notification_sent_flag = redis_con.hget(date, each_user)
+                notification_sent_set = True
+            else:
+                date = each_time_key[5:15].decode('utf-8')
+                notification_sent_flag = None
+                notification_sent_set = False
             if not notification_sent_flag:
                 hmdata = redis_con.hget('1900-%sT00:00:00+00:00'%(date), each_user)
                 data_obj = json.loads(hmdata.decode('utf-8'))
@@ -29,15 +36,24 @@ def send_notification(pattern, del_flag=False):
                     to='+1%s' % (phone, ),
                     from_='+12403033098',
                     body='Hello! Your appointment is at %s' % ('\n'.join(time_list)))
-                notification_sent = redis_con.hset(date, each_user, 1)
+                if notification_sent_set:
+                    notification_sent = redis_con.hset(date, each_user, 1)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Send notifications')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-d', '--daily', action='store_true')
+    group.add_argument('-n', '--night', action='store_true')
+    group.add_argument('-m', '--morning', action='store_true')
     args = parser.parse_args()
-    if args.daily:
+    if args.night:
         time_obj = arrow.utcnow() + datetime.timedelta(days=1)
+        pattern = time_obj.format('1900-YYYY-MM-DDT00*')
+        send_notification(pattern)
+        time_obj = arrow.utcnow() + datetime.timedelta(days=-1)
+        pattern = time_obj.format('*YYYY-MM-DD*')
+        send_notification(pattern, del_flag=True)
+    if args.morning:
+        time_obj = arrow.utcnow()
         pattern = time_obj.format('1900-YYYY-MM-DDT00*')
         send_notification(pattern)
         time_obj = arrow.utcnow() + datetime.timedelta(days=-1)
